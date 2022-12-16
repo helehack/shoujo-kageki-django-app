71bd7e7ac74a14ef5ec5617bbc88e118ac93a079
Helena Hackman <hhackman513@gmail.com>
2022-12-16 09:31:51 -0500

    Moving everything half-an-inch to the left.

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,4 +1,4 @@
-"""takawiki URL Configuration
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
